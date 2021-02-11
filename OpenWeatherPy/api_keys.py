f02cb29dfc9a58746c21863c5a4e71810c3bcd76
Jacqueline Meyer <jacquelinemeye1000@gmail.com>
2021-02-11 12:51:38 -0600

    nbconvert to config.py

---
+++ OpenWeatherPy/api_keys.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# OpenWeatherMap API Key
+weather_api_key = "91acd31ea5ad6bf23a906bf55d592e37"
+
+# Google API Key
+g_key = "AIzaSyB7g-8VwfmFDvPb_wiwnCyWFz8pBB93e0E"
+
+
+# In[ ]:
+
+
+
+
